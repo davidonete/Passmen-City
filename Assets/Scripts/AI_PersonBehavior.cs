3d5041c7491516dd5cb2653680320f9868c1ff6d
Juan Daniel Laserna Condado <condadodaniel@hotmail.com>
2017-02-05 17:42:49 +0000

    Added segments between waypoints
    Added the segments prefab and material.
    Updated the AI_PersonBehavior with some test code.

--- Assets/Scripts/AI_PersonBehavior.cs
+++ Assets/Scripts/AI_PersonBehavior.cs
@@ -2,14 +2,29 @@
 using System.Collections;
 
 public class AI_PersonBehavior : MonoBehaviour {
+	public struct Location {
+		private int x, y;
+		public int X {
+			get { return x; }
+			set { x = value; }
+		}
+		public int Y {
+			get { return y; }
+			set { y = value; }
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		//Debug.Log ("Hola");
+		Location loc = new Location();
+		loc.X = 22;
+
+		//Debug.Log (loc.X);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
