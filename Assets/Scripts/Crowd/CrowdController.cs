730a3771832722b06d15a327cfcce48ea685bf62
David Parra <davidparraausina@gmail.com>
2017-03-03 14:59:05 +0000

    Created crowd manager classes and test maps

---
+++ Assets/Scripts/Crowd/CrowdController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrowdController : MonoBehaviour
+{
+    //The time between each crowd iteration check
+    public float IterationTime;
+
+    float mCurrentIterationTime;
+    AI_PersonBehavior[] mAgents;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Init();
+	}
+
+    public void Init()
+    {
+        //Get all agents of the map and save its reference
+        mAgents = FindObjectsOfType<AI_PersonBehavior>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(mCurrentIterationTime >= IterationTime)
+        {
+            mCurrentIterationTime = 0.0f;
+            //Check crowd iteration
+            //...
+        }
+        else
+            mCurrentIterationTime += Time.deltaTime;
+	}
+}
