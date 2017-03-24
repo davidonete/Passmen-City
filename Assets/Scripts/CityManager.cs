c7b0a6c7fe24b7a4190e5bda43a345032076ec07
Daniel Laserna Condado <condadodaniel@hotmail.com>
2017-03-24 14:41:59 +0000

    Final scene now will create the city, the cars and the pedestrian.

--- Assets/Scripts/CityManager.cs
+++ Assets/Scripts/CityManager.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class CityManager : MonoBehaviour
 {
-  //Pedestrians
-  public int PedestrianAmount = 0;
-  public GameObject Pedestrian;
-
   //Cars
   public int CarAmount = 0;
   public GameObject Car;
+
+  //Pedestrians
+  public int PedestrianAmount = 0;
+  public GameObject Pedestrian;
+  public GameObject CrowdController;
 
   // Use this for initialization
   void Start()
@@ -17,28 +18,13 @@ public class CityManager : MonoBehaviour
     //Generate the city
     GetComponent<CityGenerator>().InitializeCity();
 
-    CreatePedestrians();
-
     CreateCars();
+
+    CreatePedestrians();
   }
 
   // Update is called once per frame
   void Update() { }
-
-  void CreatePedestrians()
-  {
-    //Generate the AI pedestrians
-    GameObject pedestrians = new GameObject();
-    pedestrians.name = "Pedestrians";
-
-    for (int i = 0; i < PedestrianAmount; ++i)
-    {
-      GameObject pedestrian = GameObject.Instantiate(Pedestrian);
-      pedestrian.name = "AIPedestrian_" + i;
-      pedestrian.transform.position = new Vector3(i, 0.0f, 0.0f);
-      pedestrian.transform.SetParent(pedestrians.transform);
-    }
-  }
 
   void CreateCars()
   {
@@ -53,5 +39,25 @@ public class CityManager : MonoBehaviour
       car.transform.position = new Vector3(i, 0.0f, 1);
       car.transform.SetParent(cars.transform);
     }
+  }
+
+  void CreatePedestrians()
+  {
+    //Generate the AI pedestrians
+    GameObject pedestrians = new GameObject();
+    pedestrians.name = "Pedestrians";
+
+    for (int i = 0; i < PedestrianAmount; ++i)
+    {
+      GameObject pedestrian = GameObject.Instantiate(Pedestrian);
+      pedestrian.name = "AIPedestrian_" + i;
+      pedestrian.transform.position = new Vector3(i, 0.0f, 0.0f);
+      pedestrian.transform.SetParent(pedestrians.transform);
+    }
+
+    //Create and initialize the crowd controller
+    GameObject crowdController = GameObject.Instantiate(CrowdController);
+    crowdController.name = "CrowdController";
+    crowdController.GetComponent<CrowdController>().Init();
   }
 }
