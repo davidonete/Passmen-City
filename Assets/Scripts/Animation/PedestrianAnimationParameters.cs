d26ed47bc8afdfeec2afc3498e5f045979b05ae0
Javier <javierpastorserrano@gmail.com>
2017-04-28 18:29:48 +0100

    Animations
    Signed-off-by: Javier <javierpastorserrano@gmail.com>

---
+++ Assets/Scripts/Animation/PedestrianAnimationParameters.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PedestrianAnimationParameters : MonoBehaviour {
+
+    private Animator Animator;
+    private PedestrianBehavior Pedestrian;
+
+	// Use this for initialization
+	void Start ()
+    {
+      Animator = GetComponent<Animator>();
+      Pedestrian = gameObject.GetComponent<PedestrianBehavior>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ChangeParameters();
+    }
+
+    void ChangeParameters()
+    {
+        if (Pedestrian.GetPedestrianState == PedestrianBehavior.PedestrianState.kPedestrianState_Walking)
+        {
+            Animator.SetBool("IsWalking", true);
+            Animator.SetBool("IsWaiting", false);
+        }
+        else if(Pedestrian.GetPedestrianState == PedestrianBehavior.PedestrianState.kPedestrianState_Waiting)
+        {
+            Animator.SetBool("IsWaiting", true);
+            Animator.SetBool("IsWalking", false);
+        }
+    }
+}
